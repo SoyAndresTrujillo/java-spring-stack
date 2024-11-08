5acf3967227c130ddd5fc5e7e2cccddd9f02a5f0
soyandrestrujillo <andymania.1301@gmail.com>
2024-11-08 18:51:47 -0500

    feat: update project lenguaje

---
+++ src/main/java/com/anjotics/anjotics/AnjoticsApplication.java
@@ -0,0 +1,13 @@
+package com.anjotics.anjotics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnjoticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnjoticsApplication.class, args);
+		System.out.println("Hello, world!");
+	}
+}
---
+++ src/test/java/com/anjotics/anjotics/AnjoticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjotics.anjotics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnjoticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
