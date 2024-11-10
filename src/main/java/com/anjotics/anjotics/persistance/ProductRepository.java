446c4b116ecebd029a0dcb7cd0e238745738c642
soyandrestrujillo <andymania.1301@gmail.com>
2024-11-10 12:25:28 -0500

    feat: @Repository

--- src/main/java/com/anjotics/anjotics/persistance/ProductRepository.java
+++ src/main/java/com/anjotics/anjotics/persistance/ProductRepository.java
@@ -11,15 +11,88 @@ import java.util.Optional;
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
+    /**
+     * This method is used to get all products from the database.
+     * 
+     * @return List<Product>
+     */
     public List<Product> getAll() {
+        /**
+         * In this case, the findAll method is natively implemented by Spring Data JPA
+         * and returns a List<Product>.
+         */
         return (List<Product>) productCrudRepository.findAll();
     }
 
+    /**
+     * This method is used to get all products by category from the database.
+     * 
+     * @param categoryId
+     * @return List<Product>
+     */
     public List<Product> getByCategory(int categoryId) {
+        /**
+         * In this case, the findByCategoryIdByNameAsc method is a custom query defined
+         * in the ProductCrudRepository interface and returns a List<Product>.
+         */
         return productCrudRepository.findByCategoryIdByNameAsc(categoryId);
     }
 
+    /**
+     * This method is used to get all products with less stock than the specified
+     * quantity from the database.
+     * 
+     * @param quantity
+     * @return Optional<List<Product>>
+     */
     public Optional<List<Product>> getLessStock(int quantity) {
+        /**
+         * In this case, the findByStockQuantityLessThanAndStatusTrue method is a custom
+         * query defined in the ProductCrudRepository interface and returns an
+         * Optional<List<Product>>.
+         */
         return productCrudRepository.findByStockQuantityLessThanAndStatusTrue(quantity);
+    }
+
+    /**
+     * This method is used to get a product by its id from the database.
+     * 
+     * @param productId
+     * @return Optional<Product>
+     */
+    public Optional<Product> getProduct(int productId) {
+        /**
+         * In this case, the findById method is natively implemented by Spring Data JPA
+         * and returns an Optional<Product> because the product might not exist in the
+         * database.
+         */
+        return productCrudRepository.findById(productId);
+    }
+
+    /**
+     * This method is used to save a product to the database.
+     * 
+     * @param product
+     * @return Product
+     */
+    public Product save(Product product) {
+        /**
+         * In this case, the save method is natively implemented by Spring Data JPA and
+         * returns the saved product.
+         */
+        return productCrudRepository.save(product);
+    }
+
+    /**
+     * This method is used to delete a product from the database.
+     * 
+     * @param productId
+     */
+    public void delete(int productId) {
+        /**
+         * In this case, the deleteById method is natively implemented by Spring Data
+         * JPA.
+         */
+        productCrudRepository.deleteById(productId);
     }
 }
